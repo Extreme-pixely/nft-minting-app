5cd3edab3670c39632e5ef2e272e0fa472afc465
Extreme-pixely <135835607+Extreme-pixely@users.noreply.github.com>
2023-06-08 16:30:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -472,7 +472,7 @@ function App() {
 
                       >
 
-                        SET
+                         SET
 
                       </StyledButton>
                     </s.Container>
